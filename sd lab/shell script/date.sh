730bd1bd2ccaf28addda805cc20c40c931fb206f
josephparuthickal <49152426+josephparuthickal@users.noreply.github.com>
2019-04-26 14:54:30 +0530

    Add files via upload
    sd lab programs

---
+++ sd lab/shell script/date.sh
@@ -0,0 +1,10 @@
+if [ "$1" == "monday" ]
+then
+echo "The typed argument is monday."
+elif [ "$1" == "Tuesday" ]
+then
+echo " Typed argument is Tuesday."
+else
+echo "Typed argument is neither monday nor tuesday."
+fi
+
---
+++ sd lab/shell script/while.sh
@@ -0,0 +1,7 @@
+i=11
+while test "$i" -gt 0
+do
+i=`expr $i - 1`
+echo $i
+done
+
